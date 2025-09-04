d6bf767a1324408da5f9be050f067bb180394579
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-09-04 17:20:35 +0700

    menambahkan fungsi post tambah siswa di repository data siswa

--- app/src/main/java/com/example/pamhttp/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/pamhttp/repositori/RepositoryDataSiswa.kt
@@ -3,13 +3,18 @@ package com.example.pamhttp.repositori
 import com.example.pamhttp.apiservice.ServiceApiSiswa
 import com.example.pamhttp.modeldata.DataSiswa
 
-interface RepositoryDataSiswa {
+
+interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
 
 }
 
-class JaringanRepositoryDataSiswa (
-    private val serviceApiSiswa: ServiceApiSiswa
+class JaringanRepositoryDataSiswa(
+    private  val serviceApiSiswa: ServiceApiSiswa
+
 ):RepositoryDataSiswa{
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):
+            retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
 }
