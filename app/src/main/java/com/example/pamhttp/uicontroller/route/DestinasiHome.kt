ee5b84e5166290c722aac0ab3745597b588696c8
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-09-04 17:07:39 +0700

    membuat destinasi home berisi route dan title

---
+++ app/src/main/java/com/example/pamhttp/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pamhttp.uicontroller.route
+
+import com.example.pamhttp.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
