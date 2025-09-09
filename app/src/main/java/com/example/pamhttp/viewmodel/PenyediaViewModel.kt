0a7ce01bbaaaf0102f78beb0db3719b203916721
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-09-09 16:55:02 +0700

    menambahkan initializer untuk detail view model

--- app/src/main/java/com/example/pamhttp/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pamhttp/viewmodel/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.pamhttp.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pamhttp.repositori.AplikasiDataSiswa
+import com.example.pamhttp.view.DetailSiswaScreen
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
@@ -13,5 +15,7 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer { DetailViewModel(this.createSavedStateHandle(),
+            aplikasiDataSiswa().container.repositoryDataSiswa)}
     }
 }
